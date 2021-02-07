3792a3f14aa1bf1e59e66dca02fc98d7849da837
Svetlana Stosic Milosavljevic <cecastosic@gmail.com>
2021-02-07 16:09:58 +0100

    added drawer

--- weather-app/App.js
+++ weather-app/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useDebounce } from "use-debounce";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
@@ -9,14 +10,94 @@ import { WeatherContext } from "./src/context/weatherContext";
 import { LatLonContext } from "./src/context/latLonContext";
 import useGeolocation from "react-hook-geolocation";
 import { getWeatherByLatLon } from "./src/services/index";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem
+} from "@react-navigation/drawer";
 
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function CustomDrawerContent(props) {
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItem label="Weather for:" />
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+}
+
+const DrawerNavigator = ({ cityName, setCityName }) => {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      //drawerType="slide"
+      drawerContentOptions={{
+        activeTintColor: "#fff",
+        inactiveTintColor: "#0E2A52",
+        activeBackgroundColor: "#0E2A52"
+      }}
+      drawerStyle={{
+        backgroundColor: "rgba(100, 162, 216, 0.8)"
+      }}
+      drawerContent={props => <CustomDrawerContent {...props} />}
+    >
+      <Drawer.Screen name={cityName} component={BottomTabNavigator} />
+    </Drawer.Navigator>
+  );
+};
+
+const BottomTabNavigator = () => {
+  return (
+    <Tab.Navigator
+      tabBarOptions={{
+        inactiveTintColor: "#0E2A52",
+        activeTintColor: "#fff",
+        labelStyle: { fontSize: 12 },
+        inactiveBackgroundColor: "#1B78C5",
+        activeBackgroundColor: "#0E2A52",
+        style: { borderTopWidth: 0 }
+      }}
+    >
+      <Tab.Screen
+        name="Today"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Today",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="calendar-today"
+              color={color}
+              size={size}
+            />
+          )
+        }}
+      />
+      <Tab.Screen
+        name="Weekly"
+        component={WeeklyScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="calendar-week"
+              color={color}
+              size={size}
+            />
+          )
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const App = () => {
   const geolocation = useGeolocation();
   const [cityName, setCityName] = useState("");
   const [lat, setLat] = useState(null);
   const [lon, setLon] = useState(null);
+  const [debouncedCityValue] = useDebounce(cityName, 1000);
 
   const fetchData = async (lat, lon) => {
     try {
@@ -53,44 +134,11 @@ const App = () => {
         }}
       >
         <LatLonContext.Provider value={{ lat, lon, setLat, setLon }}>
-          <Tab.Navigator
-            tabBarOptions={{
-              inactiveTintColor: "#0E2A52",
-              activeTintColor: "#fff",
-              labelStyle: { fontSize: 12 },
-              inactiveBackgroundColor: "#1B78C5",
-              activeBackgroundColor: "#0E2A52",
-              style: { borderTopWidth: 0 }
-            }}
-          >
-            <Tab.Screen
-              name="Today"
-              component={HomeScreen}
-              options={{
-                tabBarLabel: "Today",
-                tabBarIcon: ({ color, size }) => (
-                  <MaterialCommunityIcons
-                    name="calendar-today"
-                    color={color}
-                    size={size}
-                  />
-                )
-              }}
-            />
-            <Tab.Screen
-              name="Weekly"
-              component={WeeklyScreen}
-              options={{
-                tabBarIcon: ({ color, size }) => (
-                  <MaterialCommunityIcons
-                    name="calendar-week"
-                    color={color}
-                    size={size}
-                  />
-                )
-              }}
-            />
-          </Tab.Navigator>
+          <DrawerNavigator
+            cityName={
+              debouncedCityValue !== "" ? debouncedCityValue : "Copenhagen"
+            }
+          />
         </LatLonContext.Provider>
       </WeatherContext.Provider>
     </NavigationContainer>
